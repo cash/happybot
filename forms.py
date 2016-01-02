ad959ab50a41d388d848aadfacf0c29575e85fb5
cash <cash.costello@gmail.com>
2016-01-02 17:13:52 -0500

    updated index page

--- forms.py
+++ forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Email
 
 
 class SignupForm(Form):
-    user_name = StringField('Your first name', validators=[DataRequired()])
-    user_email = StringField('Your email address', validators=[Email()])
-    sender_name = StringField("Sender's name", validators=[DataRequired()])
+    user_name = TextField('Your first name', validators=[DataRequired()])
+    user_email = TextField('Your email address', validators=[Email()])
+    sender_name = TextField("Sender's name", validators=[DataRequired()])
     submit = SubmitField('Signup')
