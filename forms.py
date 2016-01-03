ff17327f6ddaeec4c12c0ce3d52e2034575ee4ce
cash <cash.costello@gmail.com>
2016-01-03 16:16:45 -0500

    form validation is now working

--- forms.py
+++ forms.py
@@ -5,6 +5,6 @@ from wtforms.validators import DataRequired, Email
 
 class SignupForm(Form):
     user_name = TextField('Your first name', validators=[DataRequired()])
-    user_email = TextField('Your email address', validators=[Email()])
+    user_email = TextField('Your email address', validators=[DataRequired(), Email()])
     sender_name = TextField("Sender's name", validators=[DataRequired()])
     submit = SubmitField('Signup')
